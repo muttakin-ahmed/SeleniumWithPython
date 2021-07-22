39991ce155d216b403d6759252750b9922993ed1
Muttakin Ahmed Khan <muttakin-ahmed@users.noreply.github.com>
2021-07-22 15:05:34 -0500

    Update testPythonFile.py

--- testPythonFile.py
+++ testPythonFile.py
@@ -1,10 +1,1 @@
-def sumNumbers(dic):
-    sm = 0
-    for i in dic.keys():
-        for j in dic[i]:
-            sm += j
-    return sm
-d = ({
-    "numbers" : [1,2,-3]
-})
-print(sumNumbers(d))
+
